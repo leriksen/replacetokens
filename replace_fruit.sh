7daff892f4a1ddf188baca1483d3a85f817b6582
Leif Eriksen <leif.eriksen.au@gmail.com>
2024-12-16 19:15:28 +1100

    fix parameters

--- replace_fruit.sh
+++ replace_fruit.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-jq --arg jq_fruit_var "${{ parameters.parameter1 }}" --argjson ip_json "${{ parameters.parameter2 }}" -f replace_filter.jq fruits_template.json > replaced.json
+jq --arg jq_fruit_var "${parameter1}" --argjson ip_json "${parameter2}" -f replace_filter.jq fruits_template.json > replaced.json
 
